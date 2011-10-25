e6d31bfd4cc295a8acf97141a82e497d7cb77a20
Andy Denenberg <andydenenberg@gmail.com>
2011-10-25 09:56:24 -0500

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments, :through => :posts
   validates_uniqueness_of :name, :case_sensitive => false
+  
+  @user = User.all  # simple, but slow = could also punch admin into db via rails Console
+     if @user.count
+       @admin = User.new()
+       @admin.name = 'Admin'
+       @admin.save
+     end
+  
 end
